b388e26390f84f80d31f161dc6a6b4699f9404b1
enjlog <enjlog@gmail.com>
2021-05-19 00:32:09 -0700

    replace the FBV with CBV

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,59 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.views import generic
 # from django.http import Http404
 
-from polls.models import Question
-
-# Create your views here.
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+from polls.models import Question, Choice
 
 
-def detail(request, question_id):
-    question_obj = get_object_or_404(Question, id=question_id)
-    # try:
-    #     question_obj = Question.objects.get(id=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    return render(request, 'polls/detail.html', {'question': question_obj})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def detail(request, question_id):
+#     question_obj = get_object_or_404(Question, id=question_id)
+#     return render(request, 'polls/detail.html', {'question': question_obj})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
-def results(request, question_id):
-    response = "You'r looking at the result of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         'question': question
+#     }
+#     return render(request, "polls/results.html", context)
+
 
 def vote(request, question_id):
-    return HttpResponse("You'r voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice!"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
